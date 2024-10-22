92173cb72c6255f61ca1bfa1abc73bb619217f73
ArtemPetyalin <a.petyalin@school.nsu.ru>
2024-10-22 22:22:34 +0700

    Здравствуйте! Сделал 10 задачу

--- task10.py
+++ task10.py
@@ -11,7 +11,7 @@
 # перевод из любой системы счисления в десятичную
 # Объедините эти две подзадачи, получите ответ.
 
-num = int(input())
+num = input()
 from_base = int(input())
 to_base = int(input())
 
@@ -20,22 +20,50 @@ def convert_base(a, b, c):
     a_str = str(a)
     #print(a_str)
 
+    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+                'V', 'W', 'X', 'Y', 'Z']
+
+    #перевод числа в десятичную систему
+
     for i in range(len(a_str)):
-        a_dec = a_dec + int(a_str[-i - 1]) * (b ** i)
+
+        for j in range(len(alphabet)):
+
+            if a_str[i] == alphabet[j]: #если цифра больше 9
+                a_dec += (j + 10) * b ** (len(a_str) - i - 1)
+
+                if j + 10 >= b:
+                    return 'error' #проверка, меньше ли цифра основания системы счисления
+                    break
+
+                break
+
+        else:
+            a_dec += int(a_str[i]) * b ** (len(a_str) - i - 1)
+
+            if int(a_str[i]) >= b:
+                return 'error' #проверка, меньше ли цифра основания системы счисления
+                break
+
+        print(a_dec)
 
 
+    #перевод в требуемую систему счисления
     a_new = ''
     razr = 1
 
     while c ** razr < a_dec:
         razr += 1
 
-    print(razr)
+    for j in range(1, razr + 1):
 
-    for j in range(razr):
-        a_new = a_new + str(a_dec // (c ** (razr - j)))
-        print(a_dec // (c ** (razr - j)))
-        a_dec -= a_dec // c ** (razr - j)
+        if a_dec // c ** (razr - j) >= 10 and c > 10:
+            a_new = a_new + alphabet[a_dec // c ** (razr - j) - 10]
+
+        else:
+            a_new = a_new + str(a_dec // c ** (razr - j))
+
+        a_dec = a_dec - (a_dec // c ** (razr - j) * c ** (razr - j))
 
     return a_new
 
